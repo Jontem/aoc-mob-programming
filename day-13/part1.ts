42ba8d7d4fd22e316bcdd4e8336027490cc3207c
Jonas Kello <jonas.kello@divid.se>
2017-12-13 18:34:20 +0100

    Parse

--- day-13/part1.ts
+++ day-13/part1.ts
@@ -1,25 +1,37 @@
 import * as fs from "fs";
 import * as path from "path";
 
-const textFile = fs.readFileSync(path.join(__dirname, "./input.txt"), {
-  encoding: "utf8"
-});
+interface LayerState {
+  readonly scannerPos: number;
+  readonly depth: number;
+}
 
-const rows = textFile.split("\n");
-
-const array: Array<number> = [];
-
-rows.forEach(r => {
-  const { depth, range } = parseRow(r);
-  array[depth] = range;
-});
+const input: string = readInput(path.join(__dirname, "./input.txt"));
+const array = parseInput(input);
 
 console.log(array);
 
+function readInput(file: string): string {
+  const textFile = fs.readFileSync(file, {
+    encoding: "utf8"
+  });
+  return textFile;
+}
+
+function parseInput(fileContent: string): ReadonlyArray<number | undefined> {
+  const rows = fileContent.split("\n");
+  const array: Array<number> = [];
+  rows.forEach(r => {
+    const { depth, range } = parseRow(r);
+    array[depth] = range;
+  });
+  return array;
+}
+
 function parseRow(r: string) {
   const parts = r.split(":");
-  const depth = parseInt(parts[0], 10);
-  const range = parseInt(parts[1], 10);
-
-  return { depth, range };
+  return {
+    depth: parseInt(parts[0], 10),
+    range: parseInt(parts[1], 10)
+  };
 }
